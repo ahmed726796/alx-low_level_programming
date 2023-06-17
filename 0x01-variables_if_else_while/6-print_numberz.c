d096eca7fd76f25a8acc8fcba28179c334e5524c
ahmed726796 <ahmedhassan726796@gmail.com>
2023-06-17 12:38:13 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+/**
+ * main - print numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 48;
+
+	for (; c < 58; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}

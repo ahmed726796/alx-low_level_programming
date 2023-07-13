e531edd0038e1476deb620c42bfc818cbfe72753
ahmed726796 <ahmedhassan726796@gmail.com>
2023-07-13 10:57:07 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - return pointer to allocated memory
+ *
+ * @b: unsigend int
+ * Return: pointer to char
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
